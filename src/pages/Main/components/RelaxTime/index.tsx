ed24aa21853efcef06f40534e7f83fee1748007d
Колосов Андрей <KolosovAA@akbarsdigital.ru>
2024-11-04 15:36:34 +0300

    Add RelaxPart to Main page

---
+++ src/pages/Main/components/RelaxTime/index.tsx
@@ -0,0 +1,68 @@
+import SectionHeader from "components/SectionHeader";
+
+import relax from "media/images/relax.png";
+import relaxOrelly from "media/images/relax_orelly.png";
+
+const RelaxTime = () => {
+  return (
+    <section>
+      <SectionHeader>Отдых</SectionHeader>
+
+      <div
+        className="
+          p-8
+          text-base
+          rounded-lg
+          bg-base-200
+          font-medium
+          flex
+          flex-col
+          lg:flex-row
+        "
+      >
+        <img
+          src={relax}
+          alt="Relax time"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+
+        <div>
+          <p>Все нормально, есть чем заняться, знаю как восстановиться</p>
+
+          <p>
+            Очень важный навык профессионала - выключать работу во время отдыха
+          </p>
+
+          <p>
+            Сейчас есть ещё одно переключение из рабочих процессов - помогаю
+            младшему сыну закончить физмат лицей. Суперзадачи, очень занятно
+          </p>
+
+          <p>
+            Летом нравится автотуризм, грибы, неведомая глушь, морские просторы.
+          </p>
+
+          <p>
+            Зимой нравится хвойный лес, мороз (в смысле, не слякоть!), шашлыки
+            под снегопадом.
+          </p>
+
+          <p>
+            Постоянно что-то почитываю, например сейчас настолка: O`Relly. Лука
+            Меццалира «Создание микрофронтендов»
+          </p>
+
+          <p className="flex flex-row justify-center align-middle">
+            <img
+              src={relaxOrelly}
+              alt="Relax time"
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RelaxTime;
